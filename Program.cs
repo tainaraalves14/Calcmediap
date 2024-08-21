fce46d41ed0de474a95ee5e5ad09b721435fc5da
Tainara Alves <tadnascimento14@gmail.com>
2024-08-21 20:58:51 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -3,7 +3,28 @@ while (resp != 'N')
 {
     Console.Clear();
     Console.WriteLine("Entre em com a nota da Prova 1 (P1): ");
-    float nota1=float.Parse(Console.ReadLine());
+    float notap1=float.Parse(Console.ReadLine());
 
-    //Esta chave fecha o WriteLine(resp!='N')
+    Console.WriteLine("Entre em com a o peso da Prova 1 (P1):");
+    float pesop1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Entre em com a nota da Prova 2 (PROJETO): ");
+    float notap2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Entre em com o peso da Prova 2 (PROJETO): ");
+    float pesop2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Entre em com a nota de Quiz: ");
+    float notaquiz = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Entre em com o peso do quiz:");
+    float pesoquiz = float.Parse(Console.ReadLine());
+
+
+    float media = ((notap1 * pesop1) + (notap2 * pesop2) + (notaquiz * pesoquiz)) / (pesop1 + pesop2 + pesoquiz);
+
+    Console.WriteLine(String.Format("A média final é {0:f2}"));
+
+
+
 }
